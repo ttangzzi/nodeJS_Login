20dabc3fa719241c744f60b2b73ec2f9a3849ac6
ttangzzi <cvbg0802@gmail.com>
2024-03-17 17:29:57 +0900

    update1

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  // 기능
+  res.render("home/index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("home/login");
+});
+
+app.listen(3000, () => {
+  console.log("서버 가동");
+});
